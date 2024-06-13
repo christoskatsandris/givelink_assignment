9a2cd1bdd4715c545549dc43461f362103a2d36c
Christos Katsandris <christoskatsandris@outlook.com>
2024-06-13 15:12:11 +0300

    Changed page title in metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Givelink Client",
+  title: "Givelink Assignment",
   description: "A webpage that shows Givelink products",
 };
 
